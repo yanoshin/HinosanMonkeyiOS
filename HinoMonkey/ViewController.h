fc79381e51e05428b3b57284be617f0c6b403119
yanoshin <yanoshin@MBP13.local>
2015-05-15 04:22:18 +0900

    Initial Commit

---
+++ HinoMonkey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HinoMonkey
+//
+//  Created by yanoshin on 5/15/15.
+//  Copyright (c) 2015 yanoshin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HinoMonkey/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HinoMonkey
+//
+//  Created by yanoshin on 5/15/15.
+//  Copyright (c) 2015 yanoshin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
